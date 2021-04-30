c1173bf53831a047d0d4378077269d5f45fc6077
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2021-04-30 16:43:07 +0200

    Fix poetry extras installation

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 service nginx start
-runuser  -m fact -c "python3 -m gunicorn.app.wsgiapp -k eventlet -b 127.0.0.1:5000 shifthelper_webinterface:app"
+runuser -m fact -c "python3 -m gunicorn.app.wsgiapp -k eventlet -b 127.0.0.1:5000 shifthelper_webinterface:app"
